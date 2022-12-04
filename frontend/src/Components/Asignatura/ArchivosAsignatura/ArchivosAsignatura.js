10de6a765a7126db62c917fdfa9b62ab154caa16
jdomingu98 <jadominguezarcas@gmail.com>
2022-12-05 00:45:56 +0100

    fallo de subirArchivo arreglado

--- frontend/src/Components/Asignatura/ArchivosAsignatura/ArchivosAsignatura.js
+++ frontend/src/Components/Asignatura/ArchivosAsignatura/ArchivosAsignatura.js
@@ -9,7 +9,7 @@ import Archivo from "./Archivo";
 import Spinner from "react-bootstrap/Spinner";
 import Button from "react-bootstrap/Button";
 import { AiOutlineUpload } from "react-icons/ai";
-import SubirArchivo from "./subirArchivo";
+import SubirArchivo from "./SubirArchivo";
 
 const PaginatedArchivos = ({ itemsPerPage, idAsignatura, idUsuario, votados }) => {
   // We start with an empty list of items.
